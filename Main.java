25594d9ecd37e5ed6e0539ddae09393cfd343d22
Sathwikreddy23 <115337601+Sathwikreddy23@users.noreply.github.com>
2025-07-21 19:35:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Nodes;
+
+public class Main {
+    public static void main(String[] args) {
+        DynamicHashMap<String, Integer> map = new DynamicHashMap<>();
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("grape", 3);
+
+        System.out.println(map.get("banana")); // 2
+        map.remove("banana");
+        System.out.println(map.get("banana")); // null
+    }
+}
