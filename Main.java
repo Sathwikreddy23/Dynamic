649b4667e6975a592dc310ba837c4408175ad25a
sathwikreddy23 <sathwikreddythummala@gmail.com>
2025-07-22 20:55:54 +0530

    adding Stream files

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Streams;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		
+		ByteFileHandler byteHandler = new ByteFileHandler();
+        byteHandler.writeAndReadByteStream();
+
+   
+        CharFileHandler charHandler = new CharFileHandler();
+        charHandler.writeAndReadCharStream();
+	}
+
+}
